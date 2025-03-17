7920b8888de9906d3945e8af4164e0dc53a3bb56
Taewook Noh <taewook.tnear@gmail.com>
2025-03-17 15:48:01 +0900

    Initial Commit

---
+++ TnearCICDTestTests/TnearCICDTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TnearCICDTestTests.swift
+//  TnearCICDTestTests
+//
+//  Created by Taewook Noh on 3/17/25.
+//
+
+import Testing
+@testable import TnearCICDTest
+
+struct TnearCICDTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
